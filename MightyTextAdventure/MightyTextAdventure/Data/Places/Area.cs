734d52377118152663ef0052eda86a5ff03d1988
Gabor Kalmar <gakalmar@gmail.com>
2024-01-09 11:47:12 +0100

    Player added to constructor parameters

--- MightyTextAdventure/MightyTextAdventure/Data/Places/Area.cs
+++ MightyTextAdventure/MightyTextAdventure/Data/Places/Area.cs
@@ -1,11 +1,14 @@
 ﻿namespace MightyTextAdventure.Data.Places;
+using MightyTextAdventure.UI;
+using MightyTextAdventure.Data.Player;
 
 public abstract class Area
 {
     public string Description { get; set; }
     public List<Area> ConnectedAreas { get; set; }
+    public Input _input;
 
-    public virtual void Interaction()
+    public virtual void Interaction(Player player)
     {
     }
 
