4445116fcaa80391d39e0790fbdb1fe85cd90fce
Gabor Kalmar <gakalmar@gmail.com>
2024-01-10 11:36:23 +0100

    Gender field added

--- MightyTextAdventure/MightyTextAdventure/Data/Player/Player.cs
+++ MightyTextAdventure/MightyTextAdventure/Data/Player/Player.cs
@@ -6,16 +6,17 @@ public class Player
     public List<string> Inventory { get; set; }
     public Area CurrentArea { get; set; }
     public string Description { get; set; }
-    
     public int HealthPoints { get; set; }
+    public string Gender { get; set; }
 
     public Player(string name, Area currentArea )
     {
         Name = name;
         Inventory = new List<string> ();
         CurrentArea = currentArea;
-        Description = $"{name}, the hero of our story. A brave warrior, who is ready to take on the world to save his beloved.";
+        Description = $"{name} is now the hero of our story. A brave warrior, who is ready to take on the world to save his/her beloved one.";
         HealthPoints = 5;
+        Gender = "";
     }
     public override string ToString()
     {
