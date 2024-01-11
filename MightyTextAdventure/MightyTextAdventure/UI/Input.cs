360c9957ec77c170cb78054aecb3f25f614490e2
Gabor Kalmar <gakalmar@gmail.com>
2024-01-11 11:53:53 +0100

    Add WaitForEnter() function

--- MightyTextAdventure/MightyTextAdventure/UI/Input.cs
+++ MightyTextAdventure/MightyTextAdventure/UI/Input.cs
@@ -23,6 +23,7 @@ public class Input
                 {
                     _display.PrintMessage("Type your choice before hitting 'Enter'!");
                 }
+                
                 if (input.ToLower() == "h" || input.ToLower() == "help")
                 {
                     PrintGameControls();
@@ -44,8 +45,31 @@ public class Input
         _display.PrintMessage($"- Enter the corresponding number to make a choice.");
         _display.PrintMessage($"- Type 'i' to check your inventory.");
         _display.PrintMessage($"- Type 'h' or 'help' to display this help message.");
+        _display.PrintMessage($"- Press `Enter` to continue, when '...' appears");
+        WaitForEnter();
     }
 
+    public void WaitForEnter()
+    {
+        Console.Write("...");
+        Console.ReadLine();
+    }
+
+    public bool CheckIfNumber(Player player, out int parsedNum)
+    {
+        string userInput;
+        do
+        {
+            userInput = this.GetInputFromUser(player);
+            if(!int.TryParse(userInput, out parsedNum))
+            {
+                _display.PrintMessage($"Please pick a number from the list!");
+                WaitForEnter();
+            }
+        } while (!int.TryParse(userInput, out parsedNum));
+
+        return true;
+    }
     private void CheckInventory(Player player)
     {
         if (player.Inventory.Count > 0)
@@ -56,6 +80,7 @@ public class Input
             {
                 _display.PrintMessage($"- {item}");
             }
+            _display.AddSeparatorLine();
         }
         else
         {
